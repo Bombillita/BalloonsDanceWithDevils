5cc15b9b618c09df46e77d8fc7828a17edecbc93
☢️Ailec Shikshaliar☣️ <151162917+AilecShikShaliar@users.noreply.github.com>
2025-03-02 13:22:19 +0100

    Test2 añadido
    penenenen

---
+++ Assets/Code/EffectsMeca/DarkArea.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkArea : MonoBehaviour
+{
+    public Renderer pRender;
+    public Collider pCollider;
+    public bool dark = true;
+    public EffectsPlayer effect;
+
+    private void Update()
+    {
+        if (effect.raycasthit == true)
+        {
+            dark = false;
+        }
+        else
+        {
+            dark = true;
+        }
+
+        if (dark == true)
+        {
+            pRender.enabled = true;
+            pCollider.enabled = true;
+        }
+        else
+        {
+            pRender.enabled = false;
+            pCollider.enabled = false;
+        }
+    }
+}
