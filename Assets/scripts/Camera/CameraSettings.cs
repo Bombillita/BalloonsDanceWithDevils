b4642132dd7cbcbee458c5762695be9ba9f7fdc6
Bombillita <elisamirasoton@gmail.com>
2025-04-03 22:35:47 +0200

    DesactivarParedes
    las paredes se desactivan (arreglado)

--- Assets/scripts/Camera/CameraSettings.cs
+++ Assets/scripts/Camera/CameraSettings.cs
@@ -6,28 +6,55 @@ public class CameraSettings : MonoBehaviour
 {
     public Transform player;
     public LayerMask obstacleLayer;
-    private Renderer dobject;
+
+    // Listas de paredes por habitación
+    public List<Renderer> habitacion1 = new List<Renderer>();
+    public List<Renderer> habitacion2 = new List<Renderer>();
+    public List<Renderer> habitacion3 = new List<Renderer>();
+
+    private List<Renderer> paredesOcultas = new List<Renderer>(); //lista de paredes ocultas
 
     void Update()
     {
         Vector3 dir = transform.position - player.position;
+        List<Renderer> nuevasParedesOcultas = new List<Renderer>();
+
+        // Lanza el Raycast
         if (Physics.Raycast(player.position, dir, out RaycastHit hit, dir.magnitude, obstacleLayer))
         {
-            if (dobject == null)
+
+            if (habitacion1.Contains(hit.collider.GetComponent<Renderer>()))
             {
-                dobject = hit.collider.GetComponent<Renderer>();
-                dobject.enabled = false;
+                nuevasParedesOcultas = habitacion1;
             }
-        }
-        else
-        {
-            if (dobject != null)
+            else if (habitacion2.Contains(hit.collider.GetComponent<Renderer>()))
             {
-                dobject.enabled = true;
-                dobject = null;
+                nuevasParedesOcultas = habitacion2;
+            }
+            else if (habitacion3.Contains(hit.collider.GetComponent<Renderer>()))
+            {
+                nuevasParedesOcultas = habitacion3;
             }
         }
 
-        Debug.DrawRay(player.position, dir, Color.yellow);
+        foreach (Renderer pared in nuevasParedesOcultas)
+        {
+            if (!paredesOcultas.Contains(pared)) // si no esta oculta
+            {
+                pared.enabled = false;
+            }
+        }
+
+        foreach (Renderer pared in paredesOcultas)
+        {
+            if (!nuevasParedesOcultas.Contains(pared)) 
+            {
+                pared.enabled = true;
+            }
+        }
+
+        paredesOcultas = nuevasParedesOcultas;
+
+        Debug.DrawRay(player.position, dir, Color.magenta);
     }
 }
